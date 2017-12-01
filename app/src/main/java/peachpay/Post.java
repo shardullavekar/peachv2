5b59da841cd2e00bf89cfeedc30fcad9f9c87ea5
Shardul <developershardul@gmail.com>
2017-12-01 18:00:35 +0530

    Changed Post methods for status and checkout id

--- app/src/main/java/peachpay/Post.java
+++ app/src/main/java/peachpay/Post.java
@@ -2,9 +2,11 @@ package peachpay;
 
 import java.io.IOException;
 
+import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -17,12 +19,38 @@ public class Post {
 
     OkHttpClient client = new OkHttpClient();
 
-    public String getId(String url, String amount, String currency, String type) throws IOException {
+    public String getId(String url, String action, String regFlag, String token, String amount, String currency, String type) throws IOException {
+        RequestBody formBody = new FormBody.Builder()
+                .add("amount", amount)
+                .add("currency", currency)
+                .add("paymentType", type)
+                .add("action", action)
+                .add("createRegistration", regFlag)
+                .add("registrations", token)
+                .build();
+
         Request request = new Request.Builder()
-                .url(url + "?action=checkout&currency=" + currency + "&type=" + type + "&amount=" + amount)
+                .url(url)
+                .post(formBody)
                 .build();
 
         Response response = client.newCall(request).execute();
         return response.body().string();
     }
+
+    public String getStatus(String url, String action, String resourcePath) throws IOException {
+        RequestBody formBody = new FormBody.Builder()
+                .add("resourcePath", resourcePath)
+                .add("action", action)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
 }
